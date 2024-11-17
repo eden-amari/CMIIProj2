2324cf5b0b825dfa5717cfd1dcad6ff7b58e8c1d
eden-amari <edenamari2005@gmail.com>
2024-11-17 14:54:51 -0500

    d

---
+++ EDEN-AMARI_WILSON_PROJ2/Assets/Enemy.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    private PlayerMovement playerMovement;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerMovement = FindFirstObjectByType<PlayerMovement>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        // Check if the collided object has the "Enemy" tag
+        if (collision.gameObject.CompareTag("Grenade") || collision.gameObject.CompareTag("Bullet"))
+        {
+            // Destroy the enemy object
+            Destroy(gameObject);
+            if (playerMovement != null)
+            {
+                playerMovement.enemiesDefeated++;
+                playerMovement.enemiesLeft--;
+                playerMovement.UpdateEnemyText();
+            }
+            else
+            {
+                Debug.LogWarning("PlayerMovement not found!");
+            }
+
+        }
+
+        // Always destroy the bullet on collision
+        Destroy(collision.gameObject);
+      
+    }
+}
